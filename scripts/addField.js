adae91ead3e7bade842e4a8c0c724532f550868c
Victor Cintra Santos <victorcs@lnxcit024157.cit>
2020-08-07 10:39:08 -0300

    Test

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,8 +1,13 @@
 document.querySelector('#add-time').addEventListener('click', cloneField)
+document.querySelector('#schedule-close-button').addEventListener('click', closeField)
 
 function cloneField() {
     const scheduleFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
     const fields = scheduleFieldContainer.querySelectorAll('input')
     fields.forEach((field) => field.value = '')
     document.querySelector('#schedule-items').appendChild(scheduleFieldContainer)
+}
+
+function closeField() {
+    console.log('teste')
 }
